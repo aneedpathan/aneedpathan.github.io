c2352bfcad143cf03dafb05c391182c2dda51105
aneedpathan <aneedpathan@gmail.com>
2025-12-13 15:29:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* script.js */
+
+// Function to open the side panel
+function openSidePanel() {
+    // Panel ko left se poora screen mein laata hai
+    document.getElementById("sidePanel").style.left = "0";
+}
+
+// Function to close the side panel
+function closeSidePanel() {
+    // Panel ko wapas left mein chupa deta hai
+    document.getElementById("sidePanel").style.left = "-100%";
+}
